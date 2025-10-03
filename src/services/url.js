c61954490e20643ea8c42fb76a484f1f30f78ebc
Mateus Kronbauer Pitta <contato.mateuspitta@gmail.com>
2025-10-03 17:34:32 -0400

    atualização de chamadas

--- src/services/url.js
+++ src/services/url.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = `api-contrato-production.up.railway.app`; //`${process.env.REACT_APP_BASE_API}`;
+const API_URL = `${process.env.REACT_APP_BASE_API}`;
 const httpsInstance = () => {
   const httpsAuthenticated = axios.create({
     baseURL: API_URL,
